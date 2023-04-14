c822daf04467cb24c258c7dd6ddd65fb2e92ce2c
tryamkin <t.shevchenko.1981@gmail.com>
2023-04-14 11:57:57 +0300

    Initial commit

---
+++ src/test/java/TestGoogle.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestGoogle {
+
+    @Test
+    public void homePage()  {
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--remote-allow-origins=*", "--headless", "--window-size=1920,1080");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.google.com/");
+        Assert.assertEquals(driver.getTitle(),"Google");
+
+        driver.quit();
+    }
+}
